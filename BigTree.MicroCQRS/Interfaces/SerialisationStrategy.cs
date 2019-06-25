4add75ef737d52b9ba17f099658233859a076f82
Paul Upson <paul.d.upson@gmail.com>
2019-06-25 18:21:15 +0100

    removal of all legacy code

--- BigTree.MicroCQRS/Interfaces/SerialisationStrategy.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BigTree.MicroCQRS {
-  public interface ISerializationStrategy {
-    string Serialize(object data);
-    object Deserialize(string data, Type type);
-  }
-}
--- BigTree.MicroCQRS/Sagas/HasCompleted.cs
+++
@@ -1,5 +0,0 @@
-namespace BigTree.MicroCQRS.Sagas {
-  public interface HasCompleted {
-    bool Completed { get; }
-  }
-}
